52abd91ce8d884a530700c80883a586781676b6e
Hoan <balaminmau@gmail.com>
2024-04-22 11:02:16 +0700

    commit 1

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Mavenproject2 {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
---
+++ src/test/java/com/mycompany/mavenproject2/Mavenproject2Test.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ACER
+ */
+public class Mavenproject2Test {
+    
+    Mavenproject2 simpleMath = new Mavenproject2();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, simpleMath.add(2, 3));
+        assertEquals(-1, simpleMath.add(2, -3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(-1, simpleMath.subtract(2, 3));
+        assertEquals(5, simpleMath.subtract(8, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, simpleMath.multiply(2, 3));
+        assertEquals(-6, simpleMath.multiply(2, -3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, simpleMath.divide(6, 3));
+        assertEquals(-2.5, simpleMath.divide(-5, 2));
+        assertThrows(IllegalArgumentException.class, () -> simpleMath.divide(6, 0));
+    }
+    
+}
